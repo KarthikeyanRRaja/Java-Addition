2560637e91a346b44a9680858b29cbb33598ac36
KarthikeyanRRaja <40626199+KarthikeyanRRaja@users.noreply.github.com>
2018-08-11 21:29:47 +0530

    Add files via upload

---
+++ class_obj_add.java
@@ -0,0 +1,13 @@
+
+public class class_obj_add {
+int a,b;
+public static void main(String ar[])
+{
+	class_obj_add ob =new class_obj_add();
+	ob.a=10;
+	ob.b=20;
+	int c=ob.a+ob.b;
+	System.out.println("The Sum of "+ob.a+"+"+ob.b+"="+c);
+	
+}
+}
